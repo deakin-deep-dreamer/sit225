dd2aa0d9aeccd70708d15afdadf873f925335b07
deakin-deep-dreamer <mahabib.deakin@gmail.com>
2024-06-18 19:03:11 +1000

    week 3 added

--- week_3/arduino_variable_sync.py
+++ week_3/arduino_variable_sync.py
@@ -1,3 +1,11 @@
+"""
+    Requirement: arduino_iot_cloud
+    Install: pip install arduino-iot-cloud
+
+    @Ahsan Habib
+    School of IT, Deakin University, Australia.
+"""
+
 import sys
 import traceback
 import random
@@ -6,28 +14,37 @@ import asyncio
 
 DEVICE_ID = "bc5c0fe9-e6ef-4eb0-90de-05032ffd9a83"
 SECRET_KEY = "3oJYfrkmSNjM4YwKGJgVObbBn"
-btn_status_active = False
 
 
-def read_value():
-    return random.randint(0, 100)
-
+# Callback function on temperature change event.
+# 
 def on_temperature_changed(client, value):
     print(f"New temperature: {value}")
 
+
 def main():
     print("main() function")
+
+    # Instantiate Arduino cloud client
     client = ArduinoCloudClient(
         device_id=DEVICE_ID, username=DEVICE_ID, password=SECRET_KEY
     )
-    # client.register("test_value")
-    # client["test_value"] = read_value()
-    client.register("temperature", value=None, on_write=on_temperature_changed)
+
+    # Register with 'temperature' cloud variable
+    # and listen on its value changes in 'on_temperature_changed'
+    # callback function.
+    # 
+    client.register(
+        "temperature", value=None, 
+        on_write=on_temperature_changed)
+
+    # start cloud client
     client.start()
+
 
 if __name__ == "__main__":
     try:
-        main()
+        main()  # main function which runs in an internal infinite loop
     except:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         traceback.print_tb(exc_type, file=print)
